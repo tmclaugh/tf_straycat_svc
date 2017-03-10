c1f87becda39d0af6bc37b71c409d3c967f0de2c
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2017-03-09 19:08:10 -0500

    Pull EC2 SSH key from IAM data source
    We can still put overdies through though.

--- data.tf
+++ data.tf
@@ -7,6 +7,15 @@ data "terraform_remote_state" "aws_vpc" {
   }
 }
 
+data "terraform_remote_state" "aws_iam" {
+  backend = "s3"
+  config = {
+    bucket  = "${var.aws_s3_prefix}-${var.aws_account}-terraform"
+    key     = "aws_iam.tfstate"
+    region  = "${var.aws_region}"
+  }
+}
+
 data "template_file" "cloud_init" {
   template = "${file("${path.module}/cloud-init.tpl")}"
 
